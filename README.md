b49bdfecc65e0a334722a95779065e329232f807
zhangke <56436057+zhangke98@users.noreply.github.com>
2020-06-29 23:31:06 +0800

    initial git

---
+++ README.md
@@ -0,0 +1,9 @@
+# ElderHelper
+A new Flutter project.
+
+version0.0 开发中
+
+针对老人日常生活的app：
+- 词云展示当日新闻关键词
+- 同城社交活动发起、参与
+- 兴趣聊天室
---
+++ android/app/src/main/kotlin/com/example/ElderHelper/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ElderHelper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
